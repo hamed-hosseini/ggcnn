07ebccd7b42046ab0c39556bfcc4846638a1be7c
hamed-hosseini <43409014+hamed-hosseini@users.noreply.github.com>
2019-12-03 16:13:34 +0330

    first commit

--- utils/data/cornell_data.py
+++ utils/data/cornell_data.py
@@ -18,9 +18,10 @@ class CornellDataset(GraspDatasetBase):
         :param kwargs: kwargs for GraspDatasetBase
         """
         super(CornellDataset, self).__init__(**kwargs)
-
+        print('this is file path:', file_path)
         graspf = glob.glob(os.path.join(file_path, '*', 'pcd*cpos.txt'))
         graspf.sort()
+        print('....', graspf)
         l = len(graspf)
         if l == 0:
             raise FileNotFoundError('No dataset files found. Check path: {}'.format(file_path))
@@ -42,12 +43,14 @@ class CornellDataset(GraspDatasetBase):
         top = max(0, min(center[0] - self.output_size // 2, 480 - self.output_size))
         return center, left, top
 
-    def get_gtbb(self, idx, rot=0, zoom=1.0):
+    def get_gtbb(self, idx, rot=0, zoom=1.0, normalise=True):
         gtbbs = grasp.GraspRectangles.load_from_cornell_file(self.grasp_files[idx])
         center, left, top = self._get_crop_attrs(idx)
         gtbbs.rotate(rot, center)
         gtbbs.offset((-top, -left))
         gtbbs.zoom(zoom, (self.output_size//2, self.output_size//2))
+        if normalise == True:
+            gtbbs.normalise()
         return gtbbs
 
     def get_depth(self, idx, rot=0, zoom=1.0):
@@ -55,7 +58,7 @@ class CornellDataset(GraspDatasetBase):
         center, left, top = self._get_crop_attrs(idx)
         depth_img.rotate(rot, center)
         depth_img.crop((top, left), (min(480, top + self.output_size), min(640, left + self.output_size)))
-        depth_img.normalise()
+        # depth_img.normalise()
         depth_img.zoom(zoom)
         depth_img.resize((self.output_size, self.output_size))
         return depth_img.img
@@ -67,7 +70,21 @@ class CornellDataset(GraspDatasetBase):
         rgb_img.crop((top, left), (min(480, top + self.output_size), min(640, left + self.output_size)))
         rgb_img.zoom(zoom)
         rgb_img.resize((self.output_size, self.output_size))
+
         if normalise:
             rgb_img.normalise()
             rgb_img.img = rgb_img.img.transpose((2, 0, 1))
         return rgb_img.img
+
+
+    def get_image(self, idx, rot=0, zoom=1.0, normalise=True):
+        rgb_img = image.Image.from_file(self.rgb_files[idx])
+        center, left, top = self._get_crop_attrs(idx)
+        rgb_img.rotate(rot, center)
+        rgb_img.crop((top, left), (min(480, top + self.output_size), min(640, left + self.output_size)))
+        rgb_img.zoom(zoom)
+        rgb_img.resize((self.output_size, self.output_size))
+        if normalise:
+            rgb_img.normalise()
+            rgb_img.img = rgb_img.img.transpose((2, 0, 1))
+        return rgb_img
--- utils/data/grasp_data.py
+++ utils/data/grasp_data.py
@@ -10,7 +10,7 @@ class GraspDatasetBase(torch.utils.data.Dataset):
     """
     An abstract dataset for training GG-CNNs in a common format.
     """
-    def __init__(self, output_size=300, include_depth=True, include_rgb=False, random_rotate=False,
+    def __init__(self, output_size=224, include_depth=False, include_rgb=True, random_rotate=False,
                  random_zoom=False, input_only=False):
         """
         :param output_size: Image output size in pixels (square)
@@ -50,13 +50,15 @@ class GraspDatasetBase(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         if self.random_rotate:
-            rotations = [0, np.pi/2, 2*np.pi/2, 3*np.pi/2]
+            rotations = [0, np.pi/4, np.pi/2, 3*np.pi/4, np.pi,  5*np.pi/4, 3*np.pi/2, 7*np.pi/4]
             rot = random.choice(rotations)
+            # rot = np.random.uniform(0, 2 * np.pi)
         else:
             rot = 0.0
 
         if self.random_zoom:
-            zoom_factor = np.random.uniform(0.5, 1.0)
+            zooms = [0.5, 0.6, 0.7, 0.8, 0.9, 1]
+            zoom_factor = random.choice(zooms)
         else:
             zoom_factor = 1.0
 
@@ -71,14 +73,17 @@ class GraspDatasetBase(torch.utils.data.Dataset):
         # Load the grasps
         bbs = self.get_gtbb(idx, rot, zoom_factor)
 
-        pos_img, ang_img, width_img = bbs.draw((self.output_size, self.output_size))
-        width_img = np.clip(width_img, 0.0, 150.0)/150.0
+        # pos_img, ang_img, width_img = bbs.draw((self.output_size, self.output_size))
+        # width_img = np.clip(width_img, 0.0, 150.0)/150.0
+
+
 
         if self.include_depth and self.include_rgb:
             x = self.numpy_to_torch(
                 np.concatenate(
-                    (np.expand_dims(depth_img, 0),
-                     rgb_img),
+                    (rgb_img[0:2, :, :],
+                     np.expand_dims(depth_img, 0)
+                     ),
                     0
                 )
             )
@@ -86,13 +91,25 @@ class GraspDatasetBase(torch.utils.data.Dataset):
             x = self.numpy_to_torch(depth_img)
         elif self.include_rgb:
             x = self.numpy_to_torch(rgb_img)
+        my_gr = random.choice(bbs.grs)
+        center_pos = my_gr.center
+        cos_angle = (np.cos(my_gr.angle) + 1) / 2
+        sin_angle = (np.sin(my_gr.angle) + 1) / 2
+        length = my_gr.length
+        width = my_gr.width
 
-        pos = self.numpy_to_torch(pos_img)
-        cos = self.numpy_to_torch(np.cos(2*ang_img))
-        sin = self.numpy_to_torch(np.sin(2*ang_img))
-        width = self.numpy_to_torch(width_img)
+        ground_truth = np.append(center_pos, cos_angle)
+        ground_truth = np.append(ground_truth, sin_angle)
+        ground_truth = np.append(ground_truth, length)
+        ground_truth = np.append(ground_truth, width)
 
-        return x, (pos, cos, sin, width), idx, rot, zoom_factor
+
+        # pos = self.numpy_to_torch(pos_img)
+        # cos = self.numpy_to_torch(np.cos(2*ang_img))
+        # sin = self.numpy_to_torch(np.sin(2*ang_img))
+        # width = self.numpy_to_torch(width_img)
+        ground_truth_torch = self.numpy_to_torch(ground_truth)
+        return x, ground_truth_torch, idx, rot, zoom_factor
 
     def __len__(self):
         return len(self.grasp_files)
